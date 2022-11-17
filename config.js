e7427d40b31f01028545e932892b850b9e4c65c1
Panzipride <118266922+Panzipride@users.noreply.github.com>
2022-11-17 18:39:19 +0800

    Update config.js

--- config.js
+++ config.js
@@ -17,14 +17,14 @@ global.APIKeys = {
 }
 
 
-global.ownername = 'Fajar Bot'
+global.ownername = 'Naya Bot'
 global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
 global.owner = ['6289684271409','6289684271409','6289684271409']
 global.premium = ['6289684271409']
-global.packname = 'Fajar Bot'
+global.packname = 'Gandi Bot'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
 global.sessionName = 'session'
